de9cc1aba7cc48b28b7191a9426d84a5d563abd2
lujunxu <lujunxu@baidu.com>
2017-11-01 16:55:08 +0800

    optimize

--- copyTableIncre.sh
+++ copyTableIncre.sh
@@ -3,7 +3,7 @@
 
 copyTable(){
 
-echo "copy table is '$COPY_USE'"
+echo -e "\e[4mcopy table is \e[31m'$COPY_USE'\e[0m\e[0m"
 if [ "$COPY_USE" == "" ];then
    echo "copy table is null ,exit";exit;
 fi
@@ -11,7 +11,7 @@ fi
 
 
 if [ "$COPY_USE" == "*" ];then
-        echo "copy all table from $HOST  to $LOCAL_HOST ";
+        echo -e "\e[4mcopy all table from \e[31m$HOST\e[0m  to \e[31m$LOCAL_HOST\e[0m \e[0m";
         COPY_USE="all table";
         STATUS='*';
 fi
@@ -21,7 +21,7 @@ COPY_TABLE=$COPY_USE
 LOCAL_TABLE=$COPY_USE
 
 
-echo "copying db '$COPY_DB'  table  '$COPY_TABLE'  to my local '$LOCAL_HOST' db '$LOCAL_DB' table '$LOCAL_TABLE'  dump"
+echo -e "\e[4mcopying from host=\e[31m$HOST\e[0m  db=\e[31m'$COPY_DB'\e[0m  table=\e[31m'$COPY_TABLE'\e[0m  to  host=\e[31m$LOCAL_HOST\e[0m db=\e[31m'$LOCAL_DB'\e[0m table=\e[31m'$LOCAL_TABLE'\e[0m  dump process---\e[0m  \r\n"
 
 
 if [ "$STATUS" == "*" ];then
@@ -29,38 +29,42 @@ if [ "$STATUS" == "*" ];then
         LOCAL_TABLE='';
 fi
 
-#test
-#nlp_rank_poi_online `source_from` = 't_column_resource' AND `rank_type` = '1' AND `m_status` = '1' ;
+
 if [ ! -z "$WHERE_COND" ];then
         #--where="1 limit 1000000"
         #WHERE `rank_type` = '1' AND `source_from` = 't_column_resource'"
-        #PROTOTYPE="$WHERE_COND";
         COND=`echo "$WHERE_COND" | sed "s/'/\"/g"`
-        echo -e "new cond is $COND  \r\n"
         COND=`echo "$COND" | sed "s/\\\`//g"`
-        echo -e "new cond is $COND  \r\n"
         #保留双引号
         #WHERE_COND='""'$WHERE_COND'""';
         WHERE_COND="$COND";
-        echo -e "the where cond is $WHERE_COND\r\n"
-        mysql -u$LOCAL_USER -p$LOCAL_PASS  -h$LOCAL_HOST -P$LOCAL_PORT $LOCAL_DB -e "delete from $LOCAL_TABLE where ""$WHERE_COND"
-        echo -e "delete from $LOCAL_HOST $LOCAL_PORT $LOCAL_DB $LOCAL_TABLE $WHERE_COND finish \r\n"
+        echo -e "the where mode : \e[1mforce update\e[0m and  cond is: \e[4m$WHERE_COND\r\n\e[0m"
+        mysql -u$LOCAL_USER -p$LOCAL_PASS  -h$LOCAL_HOST -P$LOCAL_PORT $LOCAL_DB  -e "delete from $LOCAL_TABLE where ""$WHERE_COND"
 fi
 
 #不能有空格
 SUBFFIX="  --no-create-info "
 
 if [ ! -z "$WHERE_COND" ];then
-    set -x
+    set -o pipefail
     mysqldump  --user=$USER  --password=$PASS  -h$HOST  -P$PORT --force $OPT --log-error=$ERROR \
             $COPY_DB  $COPY_TABLE  --where="$WHERE_COND"  $SUBFFIX | mysql -u$LOCAL_USER -p$LOCAL_PASS  -h$LOCAL_HOST -P$LOCAL_PORT $LOCAL_DB
-            #$COPY_DB $COPY_TABLE  $WHERE_BASE"$WHERE_COND"  $SUBFFIX >ljx.sql
-    set +x
-    else
-    set -x
+	EXITCODE=$?
+ 	if [ $EXITCODE -ne 0 ] ; then 
+      echo -e "\e[4m\r\nmysqldump failed with exit code $EXITCODE,pls see the file=$ERROR for detail \e[0m"
+ 	else
+      echo -e "\e[4m\r\nmysqldump finished \e[1mok\e[0m \e[0m"
+ 	fi
+else
+    set -o pipefail
     mysqldump  --user=$USER  --password=$PASS  -h$HOST  -P$PORT --force $OPT --log-error=$ERROR \
-            $COPY_DB $COPY_TABLE  | mysql -u$LOCAL_USER -p$LOCAL_PASS  -h$LOCAL_HOST -P$LOCAL_PORT $LOCAL_DB
-    set +x
+            $COPY_DB $COPY_TABLE  | mysql -u$LOCAL_USER -p$LOCAL_PASS  -h$LOCAL_HOST -P$LOCAL_PORT $LOCAL_DB  
+	EXITCODE=$?
+ 	if [ $EXITCODE -ne 0 ] ; then 
+      echo -e "\e[4m\r\nmysqldump failed with exit code $EXITCODE,pls see the file=$ERROR for detail \e[0m"
+ 	else
+      echo -e "\e[4m\r\nmysqldump finished \e[1mok\e[0m \e[0m"
+ 	fi
 fi
 
 }
--- sync_ab.sh
+++ sync_ab.sh
@@ -1,4 +1,4 @@
-e#!/bin/bash
+#!/bin/bash
 . copyTableIncre.sh
 
 # The following database will be DELETED first:
@@ -10,10 +10,10 @@ COPY_DB=marry
 COPY_TABLE=$COPY_USE
 USER=root
 PASS="123456"
-ERROR=/home/users/lujunxu/odp/log/mysql/duplicate_mysql_error.log
+ERROR=./mysql_error.log
 OPT=" --single-transaction  "
 
-LOCAL_HOST='10.99.xxx.xx'
+LOCAL_HOST='10.99.201.114'
 LOCAL_DB=poi
 LOCAL_PORT=8899
 LOCAL_TABLE=$COPY_USE
@@ -24,10 +24,13 @@ LOCAL_PASS="work@nuomi"
 WHERE_COND=''
 COPY_USE=''
 
+BC=$'\e[4m'
+EC=$'\e[0m'
+
 while true; do
-        read -p "Do you wish to copy table from $HOST $PORT  to $LOCAL_HOST $LOCAL_PORT ? (y or n)" yn
+        read -p "Do you wish to copy table from ${BC}$HOST $PORT${EC} to ${BC}$LOCAL_HOST $LOCAL_PORT${EC}? (y or n)" yn
     case $yn in
-        [Yy]* )echo "Please enter tablename as  input: ";
+            [Yy]* )echo -e "Please enter \e[4mtablename\e[0m as  input: : (\e[31m\e[1m*\e[0m\e[0m) is copy all source table to destination !";
                 read input_variable where_input;
                 echo "You entered: $input_variable  $where_input";
                 COPY_USE=$input_variable;
